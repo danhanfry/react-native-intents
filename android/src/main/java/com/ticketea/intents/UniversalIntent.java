e6c22f823b33e0671a2fc4c5f5a031ae0bde317a
Jorge Maroto García <patoroco@.local>
2015-10-28 13:07:36 +0100

    Create gradle config and basic java-side scaffolding

---
+++ android/src/main/java/com/ticketea/intents/UniversalIntent.java
@@ -0,0 +1,15 @@
+package com.ticketea.intents;
+
+import android.content.Intent;
+import android.net.Uri;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+
+public class UniversalIntent extends ReactContextBaseJavaModule {
+
+}
